671aeb1b5b07de21b9b50d2c7cbb2b709ff8dfc5
Sateesh Kota <sateeshkmr.kota@gmail.com>
2018-01-02 22:36:57 +0530

    Scala Functional and reduceLeft api.

--- Scala-Learning/src/main/scala/com/ksk/scala/collections/Collections.scala
+++ Scala-Learning/src/main/scala/com/ksk/scala/collections/Collections.scala
@@ -1,5 +1,11 @@
 package com.ksk.scala.collections
 
 class Collections {
+  def isOdd(x: Int) = {
+    x%2 == 1
+  }
   
+  def reduceLeft(list : List[String]) = {
+    list.reduceLeft((a,b) => if(a.length > b.length) a else b)
+  }
 }
---
+++ Scala-Learning/src/main/scala/com/ksk/scala/collections/Main.scala
@@ -0,0 +1,14 @@
+package com.ksk.scala.collections
+
+object Main extends App{
+  
+  val obj = new Collections
+  val list = 1 :: 2 :: 3 :: 4 :: 5 :: 6:: 7 :: 8 ::Nil
+  val newOddList = list.filter(obj.isOdd)
+  println("Odd Elements List = "+newOddList)
+  
+  println("99 Red Ballons".toList.filter(Character.isDigit))
+  
+  val listWords = "moose" :: "cow" :: "A" :: "cat" :: Nil
+  println("Max String is = "+obj.reduceLeft(listWords))
+}
